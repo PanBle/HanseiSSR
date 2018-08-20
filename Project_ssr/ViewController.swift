dd08741b0d32328cbc194dcf9f319e1e37725dcc
김태민 <km_tm@gimtaemin-ui-macbook-pro.local>
2018-08-20 09:48:05 +0900

    Initial Commit

---
+++ Project_ssr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project_ssr
+//
+//  Created by KM_TM on 2018. 8. 20..
+//  Copyright © 2018년 KM_TM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
